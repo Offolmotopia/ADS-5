4ec09c00ec882279e025aab02a3d914c4fb553e9
Offolmotopia <74813567+Offolmotopia@users.noreply.github.com>
2021-06-04 11:10:29 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,74 +4,57 @@
 #include <cassert>
 
 template<typename T>
-class TPQueue
-{
-  struct ITEM
-  {
+class TPQueue {
+  struct ITEM {
     T data;
     ITEM *next;
   };
 
-private:
+ private:
   ITEM *head;
   ITEM *tail;
-  ITEM *create(T data)
-  {
+  ITEM *create(T data) {
     ITEM *t = new ITEM;
     t->data = data;
     t->next = nullptr;
     return t;
   }
 
-public:
+ public:
   TPQueue() : head(nullptr), tail(nullptr) {}
-  ~TPQueue()
-  {
+  ~TPQueue() {
     while (head)
       pop();
   }
-  void push(const T &data)
-  {
-    if (tail && head)
-    {
+  void push(const T &data) {
+    if (tail && head) {
       ITEM *temp = head;
-      if (temp->data.prior < data.prior)
-      {
+      if (temp->data.prior < data.prior) {
         temp = create(data);
         temp->next = head;
         head = temp;
-      }
-      else
-      {
-        while (temp->next)
-        {
-          if (temp->next->data.prior < data.prior)
-          {
+      } else {
+        while (temp->next) {
+          if (temp->next->data.prior < data.prior) {
             ITEM *t = create(data);
             t->next = temp->next;
             temp->next = t;
             break;
-          }
-          else
-          {
+          } else {
             temp = temp->next;
           }
         }
       }
-      if (!temp->next)
-      {
+      if (!temp->next) {
         tail->next = create(data);
         tail = tail->next;
       }
-    }
-    else
-    {
+    } else {
       head = create(data);
       tail = head;
     }
   }
-  T pop()
-  {
+  T pop() {
     ITEM *temp = head->next;
     T data = head->data;
     delete head;
@@ -80,8 +63,7 @@ public:
   }
 };
 
-struct SYM
-{
+struct SYM {
   char ch;
   int prior;
 };
